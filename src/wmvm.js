f2f260e55b563a3dca4f124a188db6a5b50d9c48
wwwg <joshpauten@gmail.com>
2018-09-25 11:54:08 -0500

    detect whether or not input is binary with typing

--- src/wmvm.js
+++ src/wmvm.js
@@ -1,12 +1,21 @@
 const binaryen = require("binaryen");
 class wmvm {
     constructor(data, type) {
-        if (!data || !type) {
+        if (!data) {
             throw new TypeError("Invalid arguents for wmvm constructor");
             return;
         }
-        this.isBinary = (type === 'wasm');
         this.data = data;
+        if (type) {
+            this.isBinary = (type === 'wasm');
+        } else {
+            if (typeof data === 'string') {
+                this.isBinary = false;
+            } else if (data instanceof Buffer || data instanceof Uint8Array) {
+                this.isBinary = true;
+            }
+        }
+        
     }
 }
 Module.exports = wmvm;
