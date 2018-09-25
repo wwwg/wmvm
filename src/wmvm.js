feee461996251003f8db7f6cec351be217d7fa9f
wwwg <joshpauten@gmail.com>
2018-09-25 11:51:44 -0500

    set constructor arguments accordingly

--- src/wmvm.js
+++ src/wmvm.js
@@ -1,4 +1,12 @@
+const binaryen = require("binaryen");
 class wmvm {
-    //
+    constructor(data, type) {
+        if (!data || !type) {
+            throw new TypeError("Invalid arguents for wmvm constructor");
+            return;
+        }
+        this.isBinary = (type === 'wasm');
+        this.data = data;
+    }
 }
 Module.exports = wmvm;
