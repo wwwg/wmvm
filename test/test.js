92fb18b5404938f962ed95e0be3270ea8e36d161
wwwg <joshpauten@gmail.com>
2018-09-25 12:52:10 -0500

    properly read input tes file

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
 let wmvm = require('../src/wmvm.js');
 const fs = require('fs');
-let inData = fs.readFileSync('emcc.wasm', 'buffer');
+let inData = fs.readFileSync('test/emcc.wasm', 'binary');
 let vm = new wmvm(inData);
