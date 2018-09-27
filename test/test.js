9afec6fdc3701d27a57d4635e160b88791258dfe
wwwg <joshpauten@gmail.com>
2018-09-26 21:59:19 -0500

    add runtime function object to test.js

--- test/test.js
+++ test/test.js
@@ -1,4 +1,9 @@
 let wmvm = require('../src/wmvm.js');
+const runtime = {
+    abortStackOverflow: () => {
+        return 0;
+    }
+}
 const fs = require('fs');
 let inData = fs.readFileSync('test/emcc.wasm');
 let vm = new wmvm(inData);
