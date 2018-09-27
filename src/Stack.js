1e7902b695bbaf4d2c6f7428a3cf12f145fd321c
wwwg <joshpauten@gmail.com>
2018-09-27 18:16:24 -0500

    dont super() with this

--- src/Stack.js
+++ src/Stack.js
@@ -1,7 +1,7 @@
 // A better array designed for the vm's stack
 module.exports = class VirtualStack extends Array {
     constructor(vm) {
-        super(this);
+        super();
         this.vm = vm;
     }
 }
