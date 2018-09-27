5e69ce1b2e52322bea6c7f2c50d3462f7b5add22
wwwg <joshpauten@gmail.com>
2018-09-27 17:28:02 -0500

    Return memory pointer name and memory in getInitialMemory

--- src/getInitialMemory.js
+++ src/getInitialMemory.js
@@ -1,12 +1,29 @@
 // Locates the initial memory of the wasm application
 module.exports = (wast) => {
+    // find memory
+    let mem;
     let findMem = new RegExp(/\(data \(get_global .*\) "?.*/gim);
     let found = findMem.exec(wast);
     if (!found || !found[0])
-        return null;
-    let mem = found[0].replace(/\(data \(get_global .*\) "/gim, '');
-    mem = mem.replace('")', '');
-    mem = mem.replace(/\\00/g, '\\u0000');
-    mem = JSON.parse('"' + mem + '"');
-    return mem;
+        mem = null;
+    else {
+        mem = found[0].replace(/\(data \(get_global .*\) "/gim, '');
+        mem = mem.replace('")', '');
+        mem = mem.replace(/\\00/g, '\\u0000');
+        mem = JSON.parse('"' + mem + '"');
+    }
+    // find ptr name
+    let ptr;
+    let findPtr = new RegExp(/\(data \(get_global \$(.*)\)/gim);
+    found = findPtr.exec(wast);
+    if (!found || !found[1]) {
+        ptr = null;
+    } else {
+        ptr = found[1];
+        ptr = ptr.replace(/\).*/gim, '');
+    }
+    return {
+        memory: mem,
+        ptrName: ptr
+    };
 }
