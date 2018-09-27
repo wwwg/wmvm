082d0f1c4f4d7404de1adfc6f55b23a3f0065f46
wwwg <joshpauten@gmail.com>
2018-09-27 11:13:37 -0500

    add initial memory parser

--- src/getInitialMemory.js
+++ src/getInitialMemory.js
@@ -1,3 +1,10 @@
+// Locates the initial memory of the wasm application
 module.exports = (wast) => {
     let findMem = new RegExp(/(?<=\(data \(get_global \$memoryBase\) ")(.*?)(?=\"\))/gim);
+    let found = findMem.exec(wast);
+    if (!found[0])
+        return null;
+    let mem = found[0];
+    mem = decodeURIComponent(JSON.parse('"' + mem + '"'));
+    return mem;
 }
