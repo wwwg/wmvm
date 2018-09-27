e55a706f4b98e89a457cd53c9371e3a56318cf92
wwwg <joshpauten@gmail.com>
2018-09-27 18:18:18 -0500

    create addFrame method to stack

--- src/Stack.js
+++ src/Stack.js
@@ -1,7 +1,12 @@
+const StackFrame = require('./runtime/StackFrame');
 // A better array designed for the vm's stack
 module.exports = class VirtualStack extends Array {
     constructor(vm) {
         super();
         this.vm = vm;
+    }
+    addFrame(fn) {
+        // Before a function is interpreted, it's added to the stack
+        let frame = new StackFrame(fn);
     }
 }
