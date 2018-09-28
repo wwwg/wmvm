0168b4713c9d3b8a1a9f1f16d70c32a02cbb4ebe
wwwg <joshpauten@gmail.com>
2018-09-27 22:43:38 -0500

    add getLocal interpreter to memio module

--- src/runtime/memoryIO.js
+++ src/runtime/memoryIO.js
@@ -1,6 +1,19 @@
 const Binaryen = require('binaryen');
 let memio = {};
 memio[Binaryen.GetLocalid] = ex => {
-    //
+    let vm = ex.vm,
+        ip = ex.interpreter,
+        frame = vm.stack.currentFrame,
+        local = frame.getLocal(ex.index);
+    if (!local) {
+        vm.dbg("WARN: getLocal was called on a local that doesn't exist. Something is terribly wrong.");
+        return {
+            type: 0,
+            value: 0
+        }
+    } else {
+        local.isGetLocal = true;
+        return local;
+    }
 }
 module.exports = memio;
