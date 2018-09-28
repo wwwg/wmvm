b0e93f106decd6200cbc2b65ee70cb9cb74daebc
wwwg <joshpauten@gmail.com>
2018-09-27 22:30:45 -0500

    interpret each block child consecutively

--- src/runtime/controlFlow.js
+++ src/runtime/controlFlow.js
@@ -2,10 +2,12 @@ const Binaryen = require('binaryen'),;
 let controlFlow = {};
 controlFlow[Binaryen.BlockId] = ex => {
     let vm = ex.vm;
+    let ip = ex.interpreter;
     vm.dbg(`interpreting blk "${ex.name}"`);
     // interpret children consecutively
     for (let i = 0; i < ex.children.length; ++i) {
         let child = ex.children[i];
+        ip.interpret(child);
     }
 }
 module.exports = controlFlow;
