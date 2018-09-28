1e199f711a6e82657ae55b9944dd75839a6b08f1
wwwg <joshpauten@gmail.com>
2018-09-27 20:10:58 -0500

    add localMap to stack frame object

--- src/runtime/StackFrame.js
+++ src/runtime/StackFrame.js
@@ -2,5 +2,7 @@ const Binaryen = require('binaryen');
 module.exports = class DynamicStackFrame {
     constructor(fn) {
         this.fn = fn;
+        // map of local names to their values
+        this.localMap = {};
     }
 }
