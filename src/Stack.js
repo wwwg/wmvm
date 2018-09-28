98efa56e575234d1582dbcbb544094d3211aad2f
wwwg <joshpauten@gmail.com>
2018-09-27 21:57:20 -0500

    debug stack push/pops

--- src/Stack.js
+++ src/Stack.js
@@ -6,11 +6,13 @@ module.exports = class VirtualStack extends Array {
         this.vm = vm;
     }
     pushFrame(fn) {
+        this.vm.dbg(`stack: pushed stack frame for "${fn.name}"`);
         // Before a function is interpreted, it's added to the stack
         let frame = new StackFrame(fn);
         this.push(frame);
     }
     popFrame() {
+        this.vm.dbg(`stack: popped stack frame for "${fn.name}"`);
         return this.pop();
     }
 }
