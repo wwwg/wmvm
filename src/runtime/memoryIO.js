1d4c23325db6bc37a598dca58234e658d203eaad
wwwg <joshpauten@gmail.com>
2018-09-27 22:53:35 -0500

    add setLocal interpreter

--- src/runtime/memoryIO.js
+++ src/runtime/memoryIO.js
@@ -16,4 +16,20 @@ memio[Binaryen.GetLocalid] = ex => {
         return local;
     }
 }
+memio[Binaryen.SetLocalId] = ex => {
+    let vm = ex.vm,
+        ip = ex.interpreter,
+        frame = vm.stack.currentFrame;
+    if (ex.isTee) {
+        // todo: support this
+        vm.dbg("WARN: encountered unsupported tee SetLocal. ignoring.");
+    }
+    let res = ip.interpret(ex.value);
+    if (!res.value) {
+        vm.dbg("WARN: interpret result doesn't have a value, I can't set a local! setting it to NULL");
+        frame.setLocal(ex.index, 0x0);
+        return;
+    }
+    frame.setLocal(ex.index, res.value);
+}
 module.exports = memio;
