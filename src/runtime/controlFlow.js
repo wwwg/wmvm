1b6bba3d62118913a729c3c662c0488b78ddd8f8
wwwg <joshpauten@gmail.com>
2018-09-27 22:29:23 -0500

    index through block children

--- src/runtime/controlFlow.js
+++ src/runtime/controlFlow.js
@@ -3,5 +3,9 @@ let controlFlow = {};
 controlFlow[Binaryen.BlockId] = ex => {
     let vm = ex.vm;
     vm.dbg(`interpreting blk "${ex.name}"`);
+    // interpret children consecutively
+    for (let i = 0; i < ex.children.length; ++i) {
+        let child = ex.children[i];
+    }
 }
 module.exports = controlFlow;
