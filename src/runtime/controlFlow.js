a6813ff10d44fe11fbcb5f5759ca0c700b2b5866
wwwg <joshpauten@gmail.com>
2018-09-27 22:25:03 -0500

    add debug out for block interpreting

--- src/runtime/controlFlow.js
+++ src/runtime/controlFlow.js
@@ -1,6 +1,6 @@
 const Binaryen = require('binaryen');
 let controlFlow = {};
 controlFlow[Binaryen.BlockId] = ex => {
-    //
+    this.dbg(`interpreting blk "${this.name}"`);
 }
 module.exports = controlFlow;
