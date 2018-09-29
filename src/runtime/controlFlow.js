e80b2bb4cfb933b8498986a6d5fc7cecf406af28
wwwg <joshpauten@gmail.com>
2018-09-29 09:45:51 -0500

    disable block interpret log

--- src/runtime/controlFlow.js
+++ src/runtime/controlFlow.js
@@ -3,7 +3,7 @@ let controlFlow = {};
 controlFlow[Binaryen.BlockId] = ex => {
     let vm = ex.vm;
     let ip = ex.interpreter;
-    vm.dbg(`interpreting blk "${ex.name}"`);
+    // vm.dbg(`interpreting blk "${ex.name}"`);
     // interpret children consecutively
     for (let i = 0; i < ex.children.length; ++i) {
         let child = ex.children[i];
