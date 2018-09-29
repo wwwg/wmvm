940d679c43151ad2c8f732193390be89bf966ae9
wwwg <joshpauten@gmail.com>
2018-09-28 23:08:47 -0500

    assign globals names when parsing

--- src/parse/parseGlobals.js
+++ src/parse/parseGlobals.js
@@ -26,6 +26,7 @@ module.exports = wast => {
         if (node.type == 'Global') {
             let name = node.name.value;
             let initInstruction = node.init[0];
+            global.name = name;
             if (initInstruction.id == 'const') {
                 let initializer = initInstruction.args[0];
                 let type = toBinaryenType(initInstruction.object);
