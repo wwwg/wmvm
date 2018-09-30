fea7fd04695d7203c0fe4bbff040df39b5137111
wwwg <joshpauten@gmail.com>
2018-09-30 16:02:44 -0500

    Print stack trace in printf

--- test/test.js
+++ test/test.js
@@ -45,6 +45,7 @@ const wmvm = require('../src/wmvm.js'),
             "name": "_printf",
             "value": function(formatPtr, ptr1) {
                 // super basic printf which you should never practically use
+                this.stack.printStackTrace();
                 let format = this.memoryAccessString(formatPtr),
                     formatArg;
                 if (format.includes('%d')) {
