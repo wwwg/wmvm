af0dce64d63b0fd17ac1f56f64dad555e7538a5d
wwwg <joshpauten@gmail.com>
2018-09-29 22:55:04 -0500

    add operations module

--- src/runtime/operations.js
+++ src/runtime/operations.js
@@ -1,1 +1,11 @@
 const Binaryen = require('binaryen');
+let ops = {};
+ops[Binaryen.UnaryId] = ex => {
+    let vm = ex.vm,
+        ip = ex.interpreter;
+}
+ops[Binaryen.BinaryId] = ex => {
+    let vm = ex.vm,
+        ip = ex.interpreter;
+}
+module.exports = ops;
