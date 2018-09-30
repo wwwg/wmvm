f9d5611c6e0c01e9fbd75f39ea0d821b4ab2bfad
wwwg <joshpauten@gmail.com>
2018-09-29 23:38:45 -0500

    add if parser shell

--- src/runtime/controlFlow.js
+++ src/runtime/controlFlow.js
@@ -1,13 +1,17 @@
 const Binaryen = require('binaryen');
 let controlFlow = {};
 controlFlow[Binaryen.BlockId] = ex => {
-    let vm = ex.vm;
-    let ip = ex.interpreter;
+    let vm = ex.vm,
+        ip = ex.interpreter;
     // vm.dbg(`interpreting blk "${ex.name}"`);
     // interpret children consecutively
     for (let i = 0; i < ex.children.length; ++i) {
         let child = ex.children[i];
         ip.interpret(child);
     }
+}
+controlFlow[Binaryen.IfId] = ex => {
+    let vm = ex.vm,
+        ip = ex.interpreter;
 }
 module.exports = controlFlow;
