26df6c433647d904a9d4a55fa268ee3634263c6d
wwwg <joshpauten@gmail.com>
2018-10-01 07:09:34 -0500

    complete getFnNames module

--- src/parse/getFnNames.js
+++ src/parse/getFnNames.js
@@ -1,10 +1,14 @@
 const parse = require("@webassemblyjs/wast-parser").parse;
 module.exports = wast => {
+    let ret = [];
     wast = wast.replace(/\(import "(.*)\)/gim, '');
     let ast = parse(wast);
     let fields = ast.body[0].fields;
     for (let i = 0; i < fields.length; ++i) {
         let field = fields[i];
-        console.log(field);
+        if (field.type !== 'Func')
+            continue;
+        ret.push(field.name.value);
     }
+    return ret;
 }
