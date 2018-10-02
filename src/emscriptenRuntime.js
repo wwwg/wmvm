03687fb1db949cf48ad4cbef5af609522b103980
wwwg <joshpauten@gmail.com>
2018-10-01 20:08:38 -0500

    create emscriptenRuntime module

---
+++ src/emscriptenRuntime.js
@@ -0,0 +1,25 @@
+// A standalone runtime library for wasm binaries
+// All Emscripten-compiled libraries require Emscripten's runtime library to work, it's usually provided in the "glue" file Emscripten generates.
+module.exports = [
+    {
+        // abortStackOverflow
+    },
+    {
+        // ABORT
+    },
+    {
+        // NaN
+    },
+    {
+        // Infinity
+    },
+    {
+        // DYNAMICTOP_PTR
+    },
+    {
+        // tempDoublePtr
+    },
+    {
+        // _printf
+    }
+]
