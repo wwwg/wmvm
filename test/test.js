df8b04333b8a144736c71882c277b278a6c886af
wwwg <joshpauten@gmail.com>
2018-10-01 21:48:54 -0500

    Update test.js binary path

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 const wmvm = require('../src/wmvm.js'),
     fs = require('fs');
 
-let vm = new wmvm(fs.readFileSync('test/emcc.wasm'));
+let vm = new wmvm(fs.readFileSync('test/binaries/emcc.wasm'));
 vm.enableDebug()
     .link();
 vm.dumpMemory(1024, 30);
