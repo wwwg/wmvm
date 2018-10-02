8159f049c904d7f912d20bfb7399db7b3b2db8b7
wwwg <joshpauten@gmail.com>
2018-10-01 21:51:53 -0500

    add test1 source file

---
+++ test/src/test1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int doubleValue(int value) {
+	return value * 2;
+}
+int main() {
+	// Loop testing
+	int val;
+	while (1) {
+		val += 1;
+		if (val < 10) {
+			break;
+		}
+	}
+	printf("%s\n", "woooooo");
+	// Load / store testing
+	val = 0 + 1 + 2 + 3;
+	// Function call testing
+	int secondVal = doubleValue(val);
+	printf("secondVal: %d\n", secondVal);
+	return 0;
+}
